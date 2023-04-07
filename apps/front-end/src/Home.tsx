d5c566c3f98a10b8e45e0f0c4f43ca063044cc90
Francescoacc <accardifrancesco97@gmail.com>
2023-04-07 10:08:02 +0200

    task

--- apps/front-end/src/Home.tsx
+++ apps/front-end/src/Home.tsx
@@ -2,29 +2,71 @@ import { useEffect, useRef, useState } from "react";
 import { useQuery } from "react-query";
 import { PORT } from "../../back-end/src/constants";
 import { FakeDatum } from "../../back-end/src/types";
-
 import "./App.css";
 import { format } from "date-fns";
 
+function median(arr:number[]) {
+  if (arr.length == 0) {
+    return; // 0.
+  }
+  arr.sort((a, b) => a - b); // 1.
+  const midpoint = Math.floor(arr.length / 2); // 2.
+  const median = arr.length % 2 === 1 ?
+      arr[midpoint] : // 3.1. If odd length, just take midpoint
+      (arr[midpoint - 1] + arr[midpoint]) / 2; // 3.2. If even length, take median of midpoints
+  return median;
+}
+
 export default function Home() {
+
   const dataQuery = useQuery("repoData", () =>
     fetch(`http://localhost:${PORT}/data`).then(
       (res): Promise<FakeDatum[]> => res.json()
     )
   );
+
+  let maxAge: number = 0;
+  let minAge: number = 130;
+  let premium: number = 0;
+  let ages: number[] = [];
+  let sumOfAges = 0
+
+  dataQuery.data?.forEach(data => {
+    let years = new Date().getFullYear() - new Date(data.birthday).getFullYear()
+    sumOfAges = sumOfAges + years
+    ages.push(years)
+    if(data.subscriptionTier == "premium"){
+      premium = premium +1
+    }
+    if(maxAge < years){
+      maxAge = years
+    }
+    if(minAge > years){
+      minAge= years
+    }
+  })
+
   const renderCounterRef = useRef<number>(0);
   const renderCounterElementRef = useRef<HTMLElement>(null);
   const [[from, to], setFromTo] = useState([0, 11]);
 
   const descriptionListArray = [
     { key: "Records", value: dataQuery.data?.length },
-    { key: "Max age", value: "?" },
-    { key: "Min age", value: "?" },
-    { key: "Mean age", value: "?" },
-    { key: "Median age", value: "?" },
-    { key: "Percentage of premium", value: "?" },
-    { key: "Records", value: "?" },
+    { key: "Max age", value: maxAge },
+    { key: "Min age", value: minAge },
+    { key: "Mean age", value: sumOfAges/ages.length},
+    { key: "Median age", value: median(ages)},
+    { key: "Percentage of premium", value: (premium*100/ages.length).toString()+"%"},
   ];
+  const backGround = useRef<HTMLDivElement>(null);
+
+  const changeColor = () => {
+    if(backGround.current?.style.backgroundColor != "red" && backGround.current){
+      backGround.current.style.backgroundColor = "red"
+    } else if(backGround.current?.style.backgroundColor == "red"){
+      backGround.current.style.backgroundColor = "white"
+    }
+  }
 
   useEffect(() => {
     renderCounterRef.current = renderCounterRef.current + 1;
@@ -41,9 +83,10 @@ export default function Home() {
       </div>
     );
   }
+
   return (
-    <div>
-      <button>Change background color</button>
+    <div ref={backGround} className={"backGround"}>
+      <button onClick={() => changeColor()}>Change background color</button>
       <div>
         <dl>
           <div className="key-value">
